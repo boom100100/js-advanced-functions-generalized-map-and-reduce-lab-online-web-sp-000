7efd1ce0a44c6a9e5132fb4f3dceb4fd99903029
Bernadette Davis <bsd231@nyu.edu>
2020-01-28 23:24:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+
+function map(array, work){
+  let r = [];
+  for (let i = 0; i < array.length; i++ ) {
+    r.push(work(array[i]));
+  }
+  return r
+  
+  
+}
+
+function reduce(array, work, startingPoint=0) {
+    let r = (!!starting) ? starting : src[0];
+    let total = startingPoint;
+  for (let i = startingPoint; i < array.length; i++ ) {
+    r = work(array[i], total);
+    //console.log(Boolean(array[i]));
+  }
+  return total;
+}
+
+//function (){}
+
